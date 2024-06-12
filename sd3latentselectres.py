a1fbe51f5a37a088a117d4eda379081ed571f639
codeMODE <GavChap@users.noreply.github.com>
2024-06-12 10:53:27 +0100

    Fix errors

--- sd3latentselectres.py
+++ sd3latentselectres.py
@@ -1,11 +1,13 @@
 import os
 import json
+import comfy
+import torch
 
 
 class SD3LatentSelectRes:
     def __init__(self):
         self.device = comfy.model_management.intermediate_device()
-    
+
     @classmethod
     def INPUT_TYPES(cls):
         cls.size_sizes, cls.size_dict = read_sizes()
@@ -22,13 +24,14 @@ class SD3LatentSelectRes:
     OUTPUT_NODE = True
     CATEGORY = "generate"
 
-    def return_res(self, size_selected, multiply_factor):
+    def return_res(self, size_selected, batch_size):
         # Extract resolution name and dimensions using the key
         selected_info = self.size_dict[size_selected]
-        width = int(selected_info["width"] * multiply_factor)
-        height = int(selected_info["height"] * multiply_factor)
+        width = int(selected_info["width"])
+        height = int(selected_info["height"])
         latent = torch.ones([batch_size, 16, height // 8, width // 8], device=self.device) * 0.0609
         return (width, height, latent)
+
 
 NODE_CLASS_MAPPINGS = {
     "SD3LatentSelectRes": SD3LatentSelectRes
@@ -37,6 +40,7 @@ NODE_CLASS_MAPPINGS = {
 NODE_DISPLAY_NAME_MAPPINGS = {
     "SD3LatentSelectRes": "SD3 Select Latent Resolution"
 }
+
 
 def read_sizes():
     p = os.path.dirname(os.path.realpath(__file__))

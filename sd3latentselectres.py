1a5edd558ddd289dc433491b692478b30224257a
codeMODE <GavChap@users.noreply.github.com>
2024-10-10 20:59:40 +0100

    Update sd3latentselectres.py

--- sd3latentselectres.py
+++ sd3latentselectres.py
@@ -14,6 +14,7 @@ class SD3LatentSelectRes:
         return {
             'required': {
                 'size_selected': (cls.size_sizes,),
+                'flip_ratio': ("BOOL")
                 'batch_size': ("INT", {"default": 1, "min": 1, "max": 4096})
             }
         }
@@ -24,11 +25,16 @@ class SD3LatentSelectRes:
     OUTPUT_NODE = True
     CATEGORY = "generate/sd3"
 
-    def return_res(self, size_selected, batch_size):
+    def return_res(self, size_selected, flip_ratio, batch_size):
         # Extract resolution name and dimensions using the key
         selected_info = self.size_dict[size_selected]
-        width = int(selected_info["width"])
-        height = int(selected_info["height"])
+        if (flip_ratio):
+            width = int(selected_info["height"])
+            height = int(selected_info["width"])    
+        else:
+            width = int(selected_info["width"])
+            height = int(selected_info["height"])    
+
         latent = torch.ones([batch_size, 16, height // 8, width // 8], device=self.device) * 0.0609
         return {"width": width}, {"height": height}, {"samples": latent},
 
